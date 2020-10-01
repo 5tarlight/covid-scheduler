2f6499e0a11a034b35cac3da26c39ab4a7dd6052
Kim Kyu San <developerstarlight@gmail.com>
2020-10-01 23:06:12 +0900

    remove unused code

--- frontend/src/components/covid/CovidText.js
+++ frontend/src/components/covid/CovidText.js
@@ -7,43 +7,8 @@ import axios from 'axios'
 const cx = classNames.bind(styles)
 
 async function getCovidApi() {
-  // const url = 'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson' /*URL*/
-  // const queryParams = '?' + encodeURIComponent('ServiceKey') + '='+ serviceKey /*Service Key*/
-  //  + '&' + encodeURIComponent('ServiceKey') + '=' + encodeURIComponent('-') /**/
-  //  + '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1') /**/
-  //  + '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('10') /**/
-  //  + '&' + encodeURIComponent('startCreateDt') + '=' + encodeURIComponent('20200310') /**/
-  //  + '&' + encodeURIComponent('endCreateDt') + '=' + encodeURIComponent('20200315') /**/
-
-  // const config = {
-  //   headers: {
-  //     'Cache-Control': 'no-cache',
-  //     'Access-Control-Allow-Origin': '*',
-  //     'crossdomain': true,
-  //     withCredentials: false,
-  //   }
-  // }
-
-  // querystring.stringify({
-  //   serviceKey: serviceKey,
-  //   pageNo: 1,
-  //   numOfRows: 10
-  // })
   const url = 'http://' + server + '/api/covid/confirmed'
   return axios.get(url)
-
-  // const xhr = new XMLHttpRequest();
-  
-  
-  // await xhr.open('GET', url + queryParams)
-
-  // xhr.onreadystatechange = function () {
-  //   if (this.readyState === 4) {
-  //     console.dir('body: ' + this.responseText)
-  //   }
-  // }
-
-  // await xhr.send('')
 }
 
 class CovidText extends Component {
