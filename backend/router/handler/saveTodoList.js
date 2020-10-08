7644eeceb6e62b71c0d45730eb5ecf877de1c8e8
Kim Kyu San <developerstarlight@gmail.com>
2020-10-08 23:16:45 +0900

    bug fix

--- backend/router/handler/saveTodoList.js
+++ backend/router/handler/saveTodoList.js
@@ -1,25 +1,23 @@
 const fs = require('fs')
 const Logger = require('korean-logger')
 
+function err(error) {
+  if (error) Logger.error(error.toString())
+}
+
 module.exports.handle = (req, res, next) => {
   const ip = req.ip
   const todo = req.body.todo
   const path = `${__dirname}\\todo\\${ip.split(':').join('')}.json`
-  console.log(path)
 
   try {
     if (fs.existsSync(path)) {
-      fs.unlink(path)
+      fs.unlinkSync(path)
     }
-
-    // fs.open(path, 'w', (err, file) => {
-    //   if (err) throw err
-
-    // })
 
     fs.writeFileSync(path, JSON.stringify(todo))
   } catch (err) {
-    Logger.error(err.toString())
+    err(err)
   }
 
   res.json({
