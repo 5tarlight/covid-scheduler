72b33ae9b3e8ba0a25d46bca3ebcb85c63c985ce
Kim Kyu San <developerstarlight@gmail.com>
2020-10-18 23:22:33 +0900

    custom modal (not bootstrap, not working)

--- frontend/src/components/todo/ModifyModal/ModifyModal.js
+++ frontend/src/components/todo/ModifyModal/ModifyModal.js
@@ -1,65 +1,52 @@
-import React, { useState } from 'react'
-import { Modal, Button } from 'react-bootstrap'
+import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import deleteStyle from './ModifyModal.scss'
 import classNames from 'classnames/bind'
 
 const cx = classNames.bind(deleteStyle)
 
-const ModifyModal = ({ onModify }) => {
-  let modifyValue = React.createRef()
-  const [show, setShow] = useState(false)
+class ModifyModal extends Component {
+  render() {
+    const { onModify } = this.props
+    /*
+      #refs
+      this.modal
+     */
 
-  const handleClose = () => setShow(false)
-  const handleShow = () => {
-    setShow(true)
-  }
-  const handleKeyPress = e => {
-    if (e.key === 'Enter' && modifyValue.value.trim()) {
-      onModify(modifyValue.value.trim())
-      handleClose()
+    const handleShow = () => {
+      this.modal.style.display = 'flex'
     }
-  }
+    const handleHide = () => {
+      this.modal.style.display = 'none'
+    }
 
-  return (
-    <>
-      <Modal show={show} onHide={handleClose} onClick={e => e.stopPropagation()}>
-        <Modal.Header closeButton>
-          <Modal.Title>수정할 내용을 입력해 주세요.</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <input autoFocus className={cx('modify-input')} ref={ref => modifyValue = ref} onKeyPress={handleKeyPress} />
-        </Modal.Body>
-        <Modal.Footer>
-          <Button
-            variant='secondary' onClick={e => {
-              e.stopPropagation()
-              handleClose()
-            }}
-          >
-            취소
-          </Button>
-          <Button
-            variant='primary' onClick={e => {
-              e.stopPropagation()
-              onModify(modifyValue.value.trim())
-              handleClose()
-            }}
-          >
-            적용
-          </Button>
-        </Modal.Footer>
-      </Modal>
+    return (
+      <>
+        <div className={'modal'} onClick={e => {
+          e.stopPropagation()
+          handleHide()
+        }} ref={ref => this.modal = ref}>
+          <div className={'modal-content'}>
+            <div className={'close-bar'}>
+              <span onClick={e => {
+                e.stopPropagation()
+                handleHide()
+              }}>&times;</span>
+            </div>
+            <input placeholder={'제목'} />
+          </div>
+        </div>
 
-      <div
-        className={(cx('delete'))} onClick={e => {
+        <div
+          className={(cx('delete'))} onClick={e => {
           e.stopPropagation()
           handleShow()
         }}
-      >[수정]
-      </div>
-    </>
-  )
+        >[수정]
+        </div>
+      </>
+    )
+  }
 }
 
 export default ModifyModal
