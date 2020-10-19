d2e408e6cb915b710d5d965faaf570a5402b4f1d
Kim Kyu San <developerstarlight@gmail.com>
2020-10-19 22:52:30 +0900

    modal specification (only title working yet)

--- frontend/src/components/todo/ModifyModal/ModifyModal.js
+++ frontend/src/components/todo/ModifyModal/ModifyModal.js
@@ -11,29 +11,72 @@ class ModifyModal extends Component {
     /*
       #refs
       this.modal
+      this.title
+      this.detail
+      this.place
+      this.loc
      */
 
     const handleShow = () => {
       this.modal.style.display = 'flex'
     }
+
     const handleHide = () => {
       this.modal.style.display = 'none'
     }
 
+    const handleKeyPress = e => {
+      if (e.key === 'Enter' && this.title.value.trim()) {
+        sendModifyRequest()
+        handleHide()
+      } else if (e.keyCode === 27) { // Escape
+        console.log(e.keyCode)
+        handleHide()
+      }
+    }
+
+    const sendModifyRequest = () => {
+      onModify(this.title.value.trim())
+      // onModify(objectify())
+    }
+
+    const sendAndHideRequest = () => {
+      sendModifyRequest()
+      handleHide()
+    }
+
+    const objectify = () => {
+      return {
+        title: this.title.value.trim(),
+        detail: this.detail.value.trim(),
+        place: this.place.value.trim(),
+        loc: this.loc.value.trim()
+      }
+    }
+
     return (
       <>
-        <div className={'modal'} onClick={e => {
-          e.stopPropagation()
-          handleHide()
-        }} ref={ref => this.modal = ref}>
-          <div className={'modal-content'}>
-            <div className={'close-bar'}>
+        <div
+          className={cx('modal')}
+          onClick={e => {
+            e.stopPropagation()
+            handleHide()
+          }}
+          ref={ref => this.modal = ref}
+          onKeyDown={handleKeyPress}
+        >
+          <div className={cx('modal-content')} onClick={e => e.stopPropagation()}>
+            <div className={cx('close-bar')}>
               <span onClick={e => {
                 e.stopPropagation()
                 handleHide()
               }}>&times;</span>
             </div>
-            <input placeholder={'제목'} />
+            <input placeholder={'제목'} ref={ref => this.title = ref} />
+            <textarea className={cx('textarea-detail')} placeholder={'상세내용'} ref={ref => this.detail = ref}/>
+            <input placeholder={'장소'} ref={ref => this.place = ref} />
+            <input placeholder={'일시'} ref={ref => this.loc = ref} />
+            <button className={'save-btn'} onClick={sendAndHideRequest}>저장</button>
           </div>
         </div>
 
@@ -42,7 +85,7 @@ class ModifyModal extends Component {
           e.stopPropagation()
           handleShow()
         }}
-        >[수정]
+        >[편집]
         </div>
       </>
     )
